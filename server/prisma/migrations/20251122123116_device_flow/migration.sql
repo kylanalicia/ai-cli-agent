7b6901da2530c7be5ae80831c520b8561bc01f4a
kylanalicia <aliciakimani.ky@gmail.com>
2025-11-22 18:42:03 +0300

    Implemented OAuth device flow, including token exchage, user verfication and secure session

---
+++ server/prisma/migrations/20251122123116_device_flow/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "deviceCode" (
+    "id" TEXT NOT NULL,
+    "deviceCode" TEXT NOT NULL,
+    "userCode" TEXT NOT NULL,
+    "userId" TEXT,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL,
+    "lastPolledAt" TIMESTAMP(3),
+    "pollingInterval" INTEGER,
+    "clientId" TEXT,
+    "scope" TEXT,
+
+    CONSTRAINT "deviceCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "account_userId_idx" ON "account"("userId");
+
+-- CreateIndex
+CREATE INDEX "session_userId_idx" ON "session"("userId");
+
+-- CreateIndex
+CREATE INDEX "verification_identifier_idx" ON "verification"("identifier");
---
+++ server/src/cli/main.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import dotenv from 'dotenv';
+import chalk from 'chalk';
+import figlet from 'figlet';
+
+import { Command } from 'commander';
+import { login } from './commands/auth/login.js';
+
+dotenv.config();
+
+async function main() {
+    //Display Banner
+    console.log(
+        chalk.cyan(
+            figlet.textSync("Zyra CLI", {
+                font: "Standard",
+                horizontalLayout: "default",
+            })
+        )
+    )
+    console.log(chalk.red("A cli based AI Tool \n"));
+
+    const program = new Command("zyra");
+
+    program.version("0.0.1")
+    .description("Zyra CLI - A Cli based AI Tool")
+    .addCommand(login);
+
+    program.action(() => {
+        program.help();
+    })
+
+    program.parse()
+}
+
+main().catch((err)=>{
+    console.log(chalk.red("Error running zyra CLI: ", err));
+    process.exit(1);
+})
